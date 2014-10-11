1bcd05e4b97bbfeea54715f3ab282b10c7ebae17
Conner Charlebois <cdcharlebois@gmail.com>
2014-10-11 15:53:26 -0400

    finished static content

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -35,11 +35,20 @@
 			background-color: rgba(0,0,0,0.7);
 			color:#cee8f2;
 			font-size: 75px;
-			font-weight: 100;
+			font-weight: 200;
 			padding-left: 1em;
 			height:200px;
 			margin:0;
 			vertical-align: middle;
+		}
+		h3.title{
+			background-color: rgba(0,0,0,0.7);
+			color:#cee8f2;
+			font-weight: 200;
+			padding-left: 78px;
+			height:50px;
+			margin:0;
+			font-size: 15px
 		}
 		.header{
 			margin-top: 100px
@@ -65,6 +74,15 @@
 		p{
 			font-size: 1.6em
 		}
+		a.text-link:hover{
+			background-color:#224032;
+			color:#ffffff;
+			text-decoration: none
+		}
+		a.text-link{
+			padding:0.1em 0.2em;
+			color:#224032;
+		}
 
 	</style>
 @stop
@@ -78,6 +96,7 @@
 				</div>	
 				<div class="col-md-7">
 					<h1 class="title">Conner<br>Charlebois</h1>
+					<h3 class="title">Consultant at Adobe. Netflix Enthusiast. Coffee Aficionado</h3>
 				</div>
 			</div>	
 		</div>
@@ -87,30 +106,45 @@
 		<div class="col-md-8 col-md-offset-2">
 			<h2 class="title">About Me</h2>
 			<p class="font">
-				Lorem ipsum dolor sit amet, consectetur adip<span style="color:red">*</span>isicing elit, sed do eiusmod <span style="color:red">*</span>tempor incididunt ut labore et dolore magna aliqua.
+				Born and raised in a small town just outside of Toronto, ON, I attended (and graduated from) <a class="text-link" href="http://www.bentley.edu">Bentley University</a> in Waltham, MA. I work as a solution consultant for the Adobe Marketing Cloud, and I help change the way marketers work. When I'm not working, I dabble in web development, pretend I know how to dance, and cross my fingers that the Maple Leafs will one day make the playoffs again.
+				<!-- Lorem ipsum dolor sit amet, consectetur adip<span style="color:red">*</span>isicing elit, sed do eiusmod <span style="color:red">*</span>tempor incididunt ut labore et dolore magna aliqua. -->
 			</p>
 		</div>
 	</div>
 		<div class="row">
 			<div class="col-md-4">
 				<h2 class="title">Blog</h2>
+				<a href="/blog">
 				<div class="shortcut">
 					<img src="/assets/img/book.svg" width="134" alt="">
 				</div>
+				</a>
 			</div>
 			<div class="col-md-4">
 				<h2 class="title">Tools</h2>
-				<div class="shortcut">
-					<img src="/assets/img/wrench.svg" width="134" alt="">
-				</div>
+				<a href="http://tools.connercharlebois.com">
+					<div class="shortcut">
+						<img src="/assets/img/wrench.svg" width="134" alt="">
+					</div>
+				</a>
 			</div>
 			<div class="col-md-4">
 				<h2 class="title">Projects</h2>
-				<div class="shortcut">
-					<img src="/assets/img/case.svg" width="134" alt="">
-				</div>
+				<a href="/projects">
+					<div class="shortcut">
+						<img src="/assets/img/case.svg" width="134" alt="">
+					</div>
+				</a>
 			</div>
 		</div>
 	</div>
 	
+@stop
+
+@section('footer')
+	<script type="text/javascript" src="/assets/js/skrollr.js"></script>
+    <script type="text/javascript">
+    var s = skrollr.init();
+    </script>
+
 @stop
--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -5,12 +5,19 @@
 	<script src="/assets/js/jquery.min.js"></script>
 	<script src="/assets/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css">
+	<script type="text/javascript" src="/assets/sweetalert/sweet-alert.min.js"></script>
+	<link rel="stylesheet" href="/assets/sweetalert/sweet-alert.css">
 	@yield('header')
 </head>
 <body>
+<!-- BEGIN global header -->
+<!-- END global header -->
 	
-		@yield('content')	
-	
+		@yield('content')
+
+<!-- BEGIN global footer -->
+<br>
+<!-- END global footer -->
 </body>
 	@yield('footer')
 </html>
