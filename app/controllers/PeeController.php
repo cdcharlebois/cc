3b432a3099876a9501b944fe39aaa8d9b86caa2a
conner <cdcharlebois@gmail.com>
2014-12-09 13:20:30 -0500

    test from phpstorm

---
+++ app/controllers/PeeController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: charlebo
+ * Date: 12/9/14
+ * Time: 1:08 PM
+ */
+class PeeController extends BaseController
+{
+    public function index()
+    {
+        return "hello";
+    }
+}
